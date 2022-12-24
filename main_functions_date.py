5b84089089fbc067fb95b055bd4f560b4e5aab61
Augusto Cesar <dev.augusto.crp@gmail.com>
2022-12-23 23:50:34 -0300

    Update main_functions_date.py

--- main_functions_date.py
+++ main_functions_date.py
@@ -1,6 +1,10 @@
+from functions import today
+
 print("#########################")
 print("Qual a data de vencimento?")
 print("#########################")
 
 due_date = input("")
+
+print(today())
 
